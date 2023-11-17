00d487c0f5c213dd7179976763afc7cc54ea26cb
Zhe Shen <lucidshenzhe@gmail.com>
2023-11-17 15:42:30 +0900

    first program in GettingStarted
    Signed-off-by: Zhe Shen <lucidshenzhe@gmail.com>

---
+++ GettingStarted/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int favourite_number;
+    std::cout << "Enter your favourite number between 1 and 100: ";
+    std::cin >> favourite_number;
+    std::cout << "Amazong! That is my favourite number too!" << std::endl;
+    return 0;
+}
